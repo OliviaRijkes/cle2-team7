59ba751e4ba06a6c48bda8c9a616509bfc9d450d
OliviaRijkes <1101595@hr.nl>
2026-01-14 14:13:42 +0100

    1 slash removed from login

--- public/login.php
+++ public/login.php
@@ -48,7 +48,7 @@ if (isset($_POST['submit'])) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-    <link rel="stylesheet" href="/assets/app.css">
+    <link rel="stylesheet" href="assets/app.css">
 </head>
 <body>
 <main>
