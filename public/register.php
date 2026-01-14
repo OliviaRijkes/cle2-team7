748805259885f662b09367a2df6cf9244f830aea
OliviaRijkes <1101595@hr.nl>
2026-01-14 10:10:22 +0100

    register in public, removed auth

--- public/register.php
+++ public/register.php
@@ -2,6 +2,7 @@
 session_start();
 if ($_POST['submit']) {
     $email = htmlentities($_POST['email']);
+    $password = htmlentities($_POST['password']);
     //validate
     if ($email == "") {
         $errors['email'] = "Voer een email in";
@@ -13,26 +14,25 @@ if ($_POST['submit']) {
     }
     if (!isset($errors)) {
         //db
+        require_once __DIR__ . '/../includes/db.php';
+        //db vars
         $emailDb = mysqli_escape_string($db, $_POST['email']);
         $passwordDb = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $query = "SELECT * FROM users WHERE email = '$emailDb'";
+        $firstnameDb = mysqli_escape_string($db, $_POST['firstname']);
+        $lastnameDb = mysqli_escape_string($db, $_POST['lastname']);
+        $adminDb = mysqli_escape_string($db, $_POST['admin']);
+        //is deze persoon al geregistreerd? (later)
+        $query = "INSERT INTO users (email, password, firstname, lastname, is_admin)
+            VALUES ()";
         $result = mysqli_query($db, $query);
-        $user = mysqli_fetch_assoc($result);
-        if ($user) {
-            if (password_verify($_POST['password'], $user['password'])) {
-                //the needed data for site exploration and authorization
-                $_SESSION['user_id'] = $user['id'];
-                $_SESSION['admin'] = $user['admin'];
-                //index
-                header('Location: index.php');
-            } else {
-                $errors['password'] = "Probeer opnieuw of registreer";
-            }
+        if (!$result) {
+            $errors['password'] = "Er is een fout opgetreden";
         } else {
-            $errors['password'] = "Probeer opnieuw of registreer";
+            // login page
+            header("Location: login.php");
         }
-        mysqli_close($db);
     }
+}
 }
 ?>
 <!doctype html>
@@ -42,7 +42,7 @@ if ($_POST['submit']) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Registreer</title>
 </head>
 <body>
 
